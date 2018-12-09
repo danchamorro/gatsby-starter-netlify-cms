cd15a2bb49c8a06ea7315b1f27add9ce2c8c9f0a
danchamorro <dan@danchamorro.com>
2018-12-08 22:19:11 -0500

    Testign

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,7 +70,6 @@ module.exports = {
     {
       resolve: `gatsby-transformer-remark`,
       options: {
-        excerpt_separator: `<!-- end -->`,
         plugins: [
           {
             resolve: `gatsby-remark-prismjs`,
